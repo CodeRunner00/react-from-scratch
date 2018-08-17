e4b4af59e7846bd17f89f52fb6dc55fb0321000e
Eric Forsell <efor202@gmail.com>
2018-08-16 22:48:54 -0400

    Add dispatch action and create action

--- src/welcome.js
+++ src/welcome.js
@@ -2,18 +2,32 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const Welcome = (props) => {
-
+  console.log('in welcome ', props);
   return (
 
+    <div>
      <h1>Hello, {props.name}</h1>
+     <button onClick={props.onToggleClick}>Toggle Name</button>
+    </div>
   );
 };
 
 function mapStateToProps(state) {
   console.log('mapStateToProps ', state);
   return {
-    count: state.count
+    name: state.name
   }
 }
 
-export default connect(mapStateToProps)(Welcome);
+function mapDispatchToProps(dispatch) {
+  return {
+    onToggleClick: () => {
+    console.log('onToggleClick');
+    const action = { type: 'TOGGLE'};
+    dispatch(action);
+  }
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Welcome);
